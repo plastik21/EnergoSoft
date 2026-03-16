4f3d29d7b7bafdb4478b642aa641c45d61521380
Alexander <plastik21@users.noreply.github.com>
2026-03-16 13:43:47 +0300

    Мелкие правки

--- EnergoSoft.Server/Controllers/HistoryController.cs
+++ EnergoSoft.Server/Controllers/HistoryController.cs
@@ -2,7 +2,6 @@
 {
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
-    using Npgsql.EntityFrameworkCore.PostgreSQL;
     using System.Linq.Expressions;
 
     [ApiController]
