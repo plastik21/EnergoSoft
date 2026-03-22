708bbec7c4ea60a07f509d167eb15dcda7f2d28f
Alexander <plastik21@users.noreply.github.com>
2026-03-22 17:57:32 +0300

    Сборка солюшена для установки в докер

--- EnergoSoft.Server/Program.cs
+++ EnergoSoft.Server/Program.cs
@@ -10,19 +10,33 @@ builder.Services.AddDbContext<EnergoSoftDbContext>(options => options.UseNpgsql(
 builder.Services.AddControllers();
 builder.Services.AddOpenApi(); // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
-
-builder.Services.AddCors(options =>
+if (builder.Environment.IsDevelopment())
 {
-    options.AddDefaultPolicy(policy =>
+    // Настройка CORS
+    var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+    builder.Services.AddCors(options =>
     {
-        policy.WithOrigins(allowedOrigins ?? [])
-        .AllowAnyHeader()
-        .AllowAnyMethod();
+        options.AddDefaultPolicy(policy =>
+        {            
+            policy.WithOrigins(allowedOrigins ?? [])
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
     });
-});
+}
 
 var app = builder.Build();
+
+if (app.Environment.IsProduction())
+{
+    // Применение миграции
+    using var scope = app.Services.CreateScope();
+
+    var context = scope.ServiceProvider.GetRequiredService<EnergoSoftDbContext>();
+
+    context.Database.Migrate();
+}
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
@@ -31,10 +45,11 @@ app.MapStaticAssets();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseDeveloperExceptionPage();    
+    app.UseHttpsRedirection();
+    app.UseCors();
 }
 
-app.UseCors();
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.MapFallbackToFile("/index.html");
